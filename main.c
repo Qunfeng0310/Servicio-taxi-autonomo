208ab4dc8c59dcf1a2a3e010befaeb6edaac2e98
JuanLuisteleco <81580215+JuanLuisteleco@users.noreply.github.com>
2021-04-28 19:46:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,3 @@
-Es este apartado vamos a implementar el servicio de taxi autonomo. Por ello incluye la aplicacion asociada
-al servicio y las informaciones necesarias sobre el coche.
+En este apartado vamos a implementar el servicio de taxis autonomos. 
+Por tanto incluye la aplicacion asociada al servicio y las informaciones necesarias sobre el coche y su conexion con la aplicacion.
+  
